4d246379c524d7df4400b8903ce8b60894119693
Lee Yongjin <greattoe@gmail.com>
2021-12-29 17:35:37 +0900

    modified:   my_pkg/scripts/remote_turtlebot.py 	new file:   turtlesim_cleaner/launch/move_by_param.launch 	new file:   turtlesim_cleaner/scripts/move_by_param.py

--- my_pkg/scripts/remote_turtlebot.py
+++ my_pkg/scripts/remote_turtlebot.py
@@ -18,6 +18,8 @@ pub  = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 tw   = Twist()
 kb   = GetChar()
 
+count = 0
+
 msg  = """
 ---------------------------------------
               (forward)
@@ -41,35 +43,42 @@ while not rospy.is_shutdown():
             tw.linear.x =  tw.linear.x + LIN_STP
         else:
             tw.linear.x =  MAX_LIN_SPD
+        count = count + 1
             
     elif ch == 's':
         if  tw.linear.x - LIN_STP >= MIN_LIN_SPD:
             tw.linear.x =  tw.linear.x - LIN_STP
         else:
             tw.linear.x =  MIN_LIN_SPD
+        count = count + 1
             
     elif ch == 'a':
         if  tw.angular.z + ANG_STP <= MAX_ANG_SPD:
             tw.angular.z = tw.angular.z + ANG_STP
         else:
             tw.angular.z = MAX_ANG_SPD
+        count = count + 1
             
     elif ch == 'd':
         if  tw.angular.z - ANG_STP >= MIN_ANG_SPD:
             tw.angular.z = tw.angular.z - ANG_STP
         else:
             tw.angular.z = MIN_ANG_SPD
+        count = count + 1
             
     elif ch == ' ':
         tw.linear.x  = tw.angular.z = 0
+        count = count + 1
             
     elif ch == 'Q':
         break
         
     else:
-        pass
+        count = count + 1
         
     pub.publish(tw)
     print("linear_speed: %s, \tangular_speed: %s" %(tw.linear.x, tw.angular.z))
     
-    pub.publish(tw)
+    if count % 15 == 0:
+        count = 0
+        print(msg)
